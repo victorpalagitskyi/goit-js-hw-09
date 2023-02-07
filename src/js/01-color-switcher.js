f4733d1967248a834c643c4298dbce9e16a70bef
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-02-07 14:27:55 +0200

    push

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,5 @@
+
+const start = document.querySelector('button[data-start="Start"]');
+const stop = document.querySelector('button[data-stop="Stop"]');
+setInterval(changeColorEverySec, 1000)
+// function changeColorEverySec 
