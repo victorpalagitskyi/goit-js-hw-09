a2bee40564a741c430cc44831dac071b36a804c9
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-02-12 00:59:12 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,23 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector(".form")
-form.addEventListener("submit", startCrPr)
+const delay = document.querySelector('[name="delay"]')
+const step = document.querySelector('[name="step"]')
+const amount = document.querySelector('[name="amount"]')
+const button = document.querySelector("button")
+button.addEventListener("click", startCrPr)
 function startCrPr (e)  { 
   e.preventDefalut()
-  const {
-    elements: { delay, step,amount }
-  } = e.currentTarget;
-  console.log(delay.value)
   getData(
     Number(delay.value),
     Number(step.value),
     Number(amount.value)
   );
   function getData(delay, step, amount) { 
-    for (let position = 1; position <= amount; position + 1) { 
-      createPromise(position, delay)
+    for (let position = 1; position <= amount.value; position + 1) { 
+      createPromise(position, delay.value)
         .then(({ position, delay }) => {
-          Notify.success(
+          // Notify.success(
+          console.log(
             `✅ Fulfilled promise ${position} in ${delay}ms`
           ), {
             timeout: 3000
@@ -25,7 +26,8 @@ function startCrPr (e)  {
         .catch(({ position, delay }) => {
           Notify.success(
             (({ position, delay }) => {
-              Notiflix.Notify.failure(
+              // Notify.failure(
+              console.log(
                 `❌ Rejected promise ${position} in ${delay}ms`
               ), {
                 timeout: 3000
@@ -37,3 +39,19 @@ function startCrPr (e)  {
     delay +=step
   }
 }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      }
+      else {
+        reject("Error")
+      }
+    }, 2000)
+  })
+}
+  
+  
+  
