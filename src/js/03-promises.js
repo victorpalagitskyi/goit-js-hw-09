427b58ab5167469df8daaad9c2099ff7eb5ef1b4
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-02-13 21:43:13 +0200

    5

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector(".form")
 const delay = document.querySelector('[name="delay"]')
 const step = document.querySelector('[name="step"]')
@@ -6,39 +6,36 @@ const amount = document.querySelector('[name="amount"]')
 const button = document.querySelector("button")
 button.addEventListener("click", startCrPr)
 function startCrPr (e)  { 
-  e.preventDefalut()
+  e.preventDefault();
   getData(
     Number(delay.value),
     Number(step.value),
     Number(amount.value)
   );
-  function getData(delay, step, amount) { 
-    for (let position = 1; position <= amount.value; position + 1) { 
-      createPromise(position, delay.value)
+  function getData(delay, step, amount) {
+    for (let position = 1; position <= amount; position + 1) {
+      createPromise(position, delay)
         .then(({ position, delay }) => {
-          // Notify.success(
-          console.log(
-            `✅ Fulfilled promise ${position} in ${delay}ms`
-          ), {
-            timeout: 3000
-          }
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`,
+             {
+              timeout: 2000,
+            }
+          )
         })
         .catch(({ position, delay }) => {
-          Notify.success(
-            (({ position, delay }) => {
-              // Notify.failure(
-              console.log(
-                `❌ Rejected promise ${position} in ${delay}ms`
-              ), {
-                timeout: 3000
-              }
-            }))
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`,
+             {
+              timeout: 2000,
+            }
+          )
         })
-
+      delay += step;
     }
-    delay +=step
   }
-}
+  }
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -52,6 +49,3 @@ function createPromise(position, delay) {
     }, 2000)
   })
 }
-  
-  
-  
